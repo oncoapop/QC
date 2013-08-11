bc8a552217c52eb48127ecdde2e1d183bb15269d
Dr D Yap <oncoapop@users.noreply.github.com>
2013-08-11 02:54:56 -0700

    Extract position info from rs name

--- QC_VCF_CellLines.R
+++ QC_VCF_CellLines.R
@@ -89,8 +89,9 @@ if (check == "freq" ) names(d.frame)[2] <- "Varalfreq"
 if (check == "depth" ) names(d.frame)[2] <- "Seqdepth"
 
 for (ri in seq(len) ) 	{
-
-	d.frame$ID[ri] <- rownames(vcf_list[[rj]][ri])
+# This extracts the postion information instead of the rs name for dbSNP positions (chr_position)
+d.frame$ID[ri] <- paste(gsub("chr","",seqnames(vcf_list[[rj]][ri])), as.character(ranges(vcf_list[[rj]][ri])),sep="_")
+#	d.frame$ID[ri] <- rownames(vcf_list[[rj]][ri])
 if (check == "calls")	d.frame$Calls[ri] <- geno(vcf_list[[rj]][ri])$GT
 if (check == "freq")	d.frame$Varalfreq[ri] <- geno(vcf_list[[rj]][ri])$VF
 if (check == "depth")	d.frame$Seqdepth[ri] <- geno(vcf_list[[rj]][ri])$DP
